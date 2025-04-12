b3e6eb5b3d8d47fd025f22774490718b39ef3631
JDominguez12 <jadominguez170@gmail.com>
2025-04-11 21:33:35 -0700

    modified line 9.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     numbers = []
     previous_num = 0
     while True:
-        num = random.randint(1, 100)
+        num = random.randint(1, 10)
         if total > 100:
             total = total - previous_num
             break
